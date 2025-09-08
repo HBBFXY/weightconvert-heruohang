86de990681decd00f4060cc868a0fecb275946be
贺若航 <2359952636@qq.com>
2025-09-08 12:52:11 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@
 WeightStr = input("请输入带有单位的重量值：")
 if WeightStr[-2:] in ["kg"]:
     kg = eval(WeightStr[0:-2])
-    lb = kg * 2.2046
-    print("对应的英制重量是 {:.3f} 磅".format(lb))
+    pd = kg * 2.2046
+    print("对应的英制重量是 {:.3f} 磅".format(pd))
 elif WeightStr[-2:] in ["lb"]:
-    lb = eval(WeightStr[0:-2])
-    kg = lb / 2.2046
+    pd = eval(WeightStr[0:-2])
+    kg = pd / 2.2046
     print("对应的公制重量是 {:.3f} 千克".format(kg))
 else:
     print("输入格式错误")
