3849c987e02c03f2c531e9e65e952d3fa8ddc861
贺若航 <2359952636@qq.com>
2025-09-08 14:13:09 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if WeightStr[-2:] in ["kg"]:
     kg = eval(WeightStr[0:-2])
     pd = kg * 2.2046
     print("对应的英制重量是 {:.3f} 磅".format(pd))
-elif WeightStr[-2:] in ["lb"]:
+elif WeightStr[-2:] in ["pd"]:
     pd = eval(WeightStr[0:-2])
     kg = pd / 2.2046
     print("对应的公制重量是 {:.3f} 千克".format(kg))
