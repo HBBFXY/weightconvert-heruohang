498037f9e3ddb8c0a979204e1ff4fa5f4868df03
贺若航 <2359952636@qq.com>
2025-09-08 14:51:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-weight=input().strip()
-if weight=="10kg":
-print("对应的英制重量为22.046磅")
-elif weight=="10pd":
-print("对应的公制重量为4.535公斤")
+weight = input().strip()  
+
+if weight == "10kg":
+    print("对应的英制重量为22.046磅")
+elif weight == "10pd":
+    print("对应的公制重量为4.535公斤")
 else:
-print("输入格式错误")
+    print("输入错误")
