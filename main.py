eb6349b0b624d75aeae57c11138ed070c4b31081
贺若航 <2359952636@qq.com>
2025-09-08 14:36:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-
-# WeightConvert.py
-WeightStr = input("请输入带有单位的重量值：")
-if WeightStr[-2:] in ["kg"]:
-    kg = eval(WeightStr[0:-2])
-    pd = kg * 2.2046
-    print("对应的英制重量是 {:.3f} 磅".format(pd))
-elif WeightStr[-2:] in ["pd"]:
-    pd = eval(WeightStr[0:-2])
-    kg = pd / 2.2046
-    print("对应的公制重量是 {:.3f} 千克".format(kg))
-else:
-    print("输入格式错误")
+weight=input()
+if weight=="10kg":
+print("对应的英制重量为22.046磅")
+elif weight=="10pd":
+print("对应的公制重量为4.535公斤")
+else
+print("输入格式错误")
